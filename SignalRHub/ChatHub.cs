52b9d8994a2ff9046fecd1c4fa1b1bac6fd328e3
Minh Nguyen <ndminh1010@gmail.com>
2025-12-17 11:26:25 +0700

    Initial project

---
+++ SignalRHub/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace signalR.SignalRHub
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageAsync(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
